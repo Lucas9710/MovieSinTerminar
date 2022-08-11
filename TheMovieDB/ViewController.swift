8a4e8c13dcaecd94b646ea67bd593fce79418506
Lucas Nunez <lucas.9710@hotmail.com>
2022-08-11 16:40:04 -0300

    Initial Commit

---
+++ TheMovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Lucas on 11/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
